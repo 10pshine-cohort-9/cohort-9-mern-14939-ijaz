b7068209a9846a303d5a7af9525e418c3216121d
IjazUllahKhan <ijazullahkhan09@gmail.com>
2026-07-30 16:27:56 +0500

    chore: address CodeRabbit review feedback

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -14,7 +14,7 @@ export default [
 
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.json", "./tsconfig.prisma.json"],
+        project: "./tsconfig.json",
       },
     },
 
